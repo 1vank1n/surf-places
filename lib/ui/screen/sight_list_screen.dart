c9ef9ffb52ae3fa909bd26065254544e4d452f1e
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-03-09 20:15:29 +0300

    add AppBar

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,7 +10,50 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Список интересных мест'),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(72.0),
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: RichText(
+              text: const TextSpan(
+                style: TextStyle(
+                  color: Color(0xFF3B3E5B),
+                  fontFamily: 'Roboto',
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  height: 36 / 32,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'С',
+                    style: TextStyle(
+                      color: Color(0xFF4CAF50),
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'писок\n',
+                  ),
+                  TextSpan(
+                    text: 'и',
+                    style: TextStyle(
+                      color: Color(0xFFFCDD3D),
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'нтересных мест',
+                  ),
+                ],
+              ),
+            ),
+            // (
+            //   maxLines: 2,
+            //   textAlign: TextAlign.left,
+            // ),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
     );
   }
