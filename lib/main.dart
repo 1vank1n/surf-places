a374f2480925fe4e87d81cd8c4aaf7f8e99be51b
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-03-10 14:09:15 +0300

    SightListScreen created (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
 }
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyFirstWidget(),
-//     );
-//   }
-// }
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App title',
-      home: MyFirstWidget(),
-    );
-  }
-}
-
-class MyFirstWidget extends StatefulWidget {
-  @override
-  _MyFirstWidgetState createState() => _MyFirstWidgetState();
-}
-
-class _MyFirstWidgetState extends State<MyFirstWidget> {
-  int _countBuild = 0;
-
-  void _incrementCountBuild() {
-    setState(() {
-      _countBuild++;
-    });
-
-    print('Count build: $_countBuild');
-  }
-
-  Type _getContextRuntimeType() {
-    return context.runtimeType;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _incrementCountBuild();
-    print(_getContextRuntimeType());
-
-    return Container(
-      child: Center(
-        child: Text('Hello state'),
-      ),
-    );
-  }
-}
-
-class MyFirstStatelessWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Type _getContextRuntimeType() {
-      return context.runtimeType;
-    }
-
-    print(_getContextRuntimeType());
-
-    return Container(
-      child: Center(
-        child: Text('Goodbye state'),
-      ),
+      title: 'Surf Places',
+      home: SightListScreen(),
     );
   }
 }
