f51cd98aad2e9feaeeafce8d5f1d7cb8c93b5613
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-03-10 15:13:29 +0300

    4.3-text (#6)
    * SightListScreen created
    
    * add AppBar
    
    * rollback to Text widget
    
    * extract common TextStyle to `lib/ui/res`

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/res/text_styles.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
@@ -10,8 +11,23 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Список интересных мест'),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(72.0),
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'Список\nинтересных мест',
+              style: headingTextStyle,
+              maxLines: 2,
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
+      backgroundColor: Colors.white,
     );
   }
 }
