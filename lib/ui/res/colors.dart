a8f972c82449edc9b30ecdec3adb38020cbb48f6
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-03-12 14:53:20 +0300

    4.6-3 Added SightDetailsScreeen (#10)
    * add Sight, mocks
    
    * add SightCard
    
    * add SightDetailsScreen
    
    * update colors

--- lib/ui/res/colors.dart
+++ lib/ui/res/colors.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 // common
 const Color primaryColor = Color(0xFF3B3E5B);
+const Color successColor = Color(0xFF4CAF50);
 
 // bg
 Color primaryBgColor = Colors.grey.shade500;
 Color secondaryBgColor = Colors.grey.shade100;
+
+// elements
+const Color dividerColor = Color.fromRGBO(124, 126, 146, 0.56);
 
 // text
 const Color primaryTextColor = primaryColor;
---
+++ lib/ui/screen/sight_details_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
+import 'package:places/mocks.dart';
+import 'package:places/ui/res/colors.dart';
+import 'package:places/ui/res/text_styles.dart';
+
+class SightDetailsScreen extends StatelessWidget {
+  final Sight sight = mocks.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  color: primaryBgColor,
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.width,
+                ),
+                Positioned(
+                  top: 36.0,
+                  left: 16.0,
+                  child: SafeArea(
+                    child: Container(
+                      width: 36.0,
+                      height: 36.0,
+                      color: primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    sight.name,
+                    style: titleTextStyle,
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        sight.type.toLowerCase(),
+                        style: smallBoldTextStyle,
+                      ),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      Text(
+                        'закрыто до 09:00',
+                        style: smallTextStyle,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Text(
+                    sight.details,
+                    style: smallTextStyle,
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  SizedBox(
+                    height: 48.0,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: successColor,
+                        elevation: 0,
+                      ),
+                      child: Text(
+                        'ПОСТРОИТЬ МАРШРУТ',
+                        style: buttonTextStyle,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Divider(
+                    color: dividerColor,
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 40.0,
+                          child: Opacity(
+                            opacity: 0.56,
+                            child: TextButton(
+                              child: Text(
+                                'Запланировать',
+                                style: smallTextStyle.copyWith(
+                                  color: primaryColor,
+                                ),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          height: 40.0,
+                          child: TextButton(
+                            child: Text(
+                              'В Избранное',
+                              style: smallTextStyle.copyWith(
+                                color: primaryColor,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
