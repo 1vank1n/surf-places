db35e1c996cf6a722c02e065a98a00ae33685def
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-03-12 14:34:26 +0300

    4.6-1 add Sight, mocks (#8)
    * add Sight, mocks
    
    * turn on null-safety

---
+++ lib/domain/sight.dart
@@ -0,0 +1,19 @@
+class Sight {
+  // Достопримечательность. Основной класс для формирования фида
+
+  String name;
+  String lat;
+  String lon;
+  String url;
+  String details;
+  String type;
+
+  Sight({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.url,
+    required this.details,
+    required this.type,
+  });
+}
