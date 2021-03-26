b2d4b0d3d44c8db12d3939094ecfa13fe7d01e97
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-03-26 11:37:30 +0300

    add border radius clip for vistied card

--- lib/ui/common/widgets/sight_visited_card.dart
+++ lib/ui/common/widgets/sight_visited_card.dart
@@ -18,110 +18,113 @@ class SightVisitedCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
-      child: Stack(
-        children: [
-          AspectRatio(
-            aspectRatio: 3 / 2,
-            child: Container(
-              width: double.infinity,
-              child: Image.network(
-                sight.url,
-                fit: BoxFit.cover,
-                loadingBuilder:
-                    (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) return child;
-                  return Center(
-                    child: CupertinoActivityIndicator.partiallyRevealed(
-                      progress: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                          : 1,
-                    ),
-                  );
-                },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16.0),
+        child: Stack(
+          children: [
+            AspectRatio(
+              aspectRatio: 3 / 2,
+              child: Container(
+                width: double.infinity,
+                child: Image.network(
+                  sight.url,
+                  fit: BoxFit.cover,
+                  loadingBuilder:
+                      (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CupertinoActivityIndicator.partiallyRevealed(
+                        progress: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                            : 1,
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 16.0,
-            left: 16.0,
-            child: Text(
-              sight.type.toLowerCase(),
-              style: Theme.of(context).textTheme.headline4!.copyWith(color: Colors.white),
+            Positioned(
+              top: 16.0,
+              left: 16.0,
+              child: Text(
+                sight.type.toLowerCase(),
+                style: Theme.of(context).textTheme.headline4!.copyWith(color: Colors.white),
+              ),
             ),
-          ),
-          Positioned(
-            top: 19.0,
-            right: 18.0,
-            child: Row(
-              children: [
-                Container(
-                  width: 24.0,
-                  height: 24.0,
-                  child: IconButton(
-                    onPressed: () {
-                      print('Pressed share button');
-                    },
-                    padding: EdgeInsets.zero,
-                    icon: SvgPicture.asset(
-                      iconShare,
-                      color: Colors.white,
-                      width: 24.0,
-                      height: 24.0,
-                    ),
-                  ),
-                ),
-                SizedBox(width: 16.0),
-                Container(
-                  width: 24.0,
-                  height: 24.0,
-                  child: IconButton(
-                    onPressed: () {
-                      removeHandler();
-                    },
-                    padding: EdgeInsets.zero,
-                    icon: Icon(
-                      Icons.close,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            right: 0,
-            bottom: 0,
-            left: 0,
-            child: Container(
-              color: Theme.of(context).secondaryHeaderColor,
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Positioned(
+              top: 19.0,
+              right: 18.0,
+              child: Row(
                 children: [
-                  Text(
-                    sight.name,
-                    style: Theme.of(context).textTheme.headline4,
+                  Container(
+                    width: 24.0,
+                    height: 24.0,
+                    child: IconButton(
+                      onPressed: () {
+                        print('Pressed share button');
+                      },
+                      padding: EdgeInsets.zero,
+                      icon: SvgPicture.asset(
+                        iconShare,
+                        color: Colors.white,
+                        width: 24.0,
+                        height: 24.0,
+                      ),
+                    ),
                   ),
-                  SizedBox(
-                    height: 2.0,
-                  ),
-                  Text(
-                    'Цель достигнута 12 окт. 2020',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  SizedBox(
-                    height: 12.0,
-                  ),
-                  Text(
-                    'закрыто до 09:00',
-                    style: Theme.of(context).textTheme.bodyText2,
+                  SizedBox(width: 16.0),
+                  Container(
+                    width: 24.0,
+                    height: 24.0,
+                    child: IconButton(
+                      onPressed: () {
+                        removeHandler();
+                      },
+                      padding: EdgeInsets.zero,
+                      icon: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-          )
-        ],
+            Positioned(
+              right: 0,
+              bottom: 0,
+              left: 0,
+              child: Container(
+                color: Theme.of(context).secondaryHeaderColor,
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      sight.name,
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    SizedBox(
+                      height: 2.0,
+                    ),
+                    Text(
+                      'Цель достигнута 12 окт. 2020',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      'закрыто до 09:00',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
