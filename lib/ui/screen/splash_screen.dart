658caae6b20ed3593b166a4996c9153a08ab151f
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-04-07 10:14:51 +0300

    7.2 Added splash screen (#33)
    * add splash screen
    
    * refactor Timer to Future.delayed

---
+++ lib/ui/screen/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:places/ui/res/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  static const TIMEOUT_SECONDS = 2;
+  late Future isInitialized;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+    _navigateToNext();
+  }
+
+  void _navigateToNext() {
+    Future.delayed(Duration(seconds: TIMEOUT_SECONDS), () {
+      print('Navigate to next screen');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            warningColor,
+            successColor,
+          ],
+        ),
+      ),
+      child: Center(
+        child: SvgPicture.asset('res/logo.svg'),
+      ),
+    );
+  }
+}
