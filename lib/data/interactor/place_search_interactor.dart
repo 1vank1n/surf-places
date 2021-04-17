577e9124003bd37a273eaea0c01a314fb60311fe
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-04-17 11:21:49 +0300

    implement search logic

---
+++ lib/data/interactor/place_search_interactor.dart
@@ -0,0 +1,34 @@
+import 'package:places/data/model/place.dart';
+import 'package:places/data/model/places_filter_request_dto.dart';
+import 'package:places/data/repository/place_respository.dart';
+
+class PlaceSearchInteractor {
+  static List<String> queries = [];
+
+  static Future<List<Place>> searchPlaces(String name) {
+    var _filter = PlacesFilterRequestDto(nameFilter: name);
+    addQuery(name);
+    return PlaceRepository.postFilteredPlaces(_filter);
+  }
+
+  static void addQuery(String query) {
+    if (query != '') {
+      if (!queries.contains(query)) {
+        queries.add(query);
+      } else {
+        queries.remove(query);
+        queries.insert(0, query);
+      }
+    }
+  }
+
+  static void removeQuery(String query) {
+    if (queries.contains(query)) {
+      queries.remove(query);
+    }
+  }
+
+  static void cleanQueries() {
+    queries.clear();
+  }
+}
