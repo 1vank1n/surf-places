b8d82f58f93d686729f8eeb9c0eabf69a3f88004
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2021-04-20 10:07:12 +0300

    refactor PlaceRepository to non static methods

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -34,8 +34,6 @@ class _SightListScreenState extends State<SightListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
-
     return Scaffold(
       body: OrientationBuilder(
         builder: (BuildContext context, Orientation orientation) {
